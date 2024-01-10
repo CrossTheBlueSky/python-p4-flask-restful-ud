0aa1825f3d403f76552619c6049c9c504cc32837
Derek Mercedes <mercedes.derek@gmail.com>
2024-01-10 10:25:28 -0700

    PATCH and DELETE

--- server/app.py
+++ server/app.py
@@ -79,6 +79,22 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self, id):
+        newsletter = Newsletter.query.filter(Newsletter.id == id)
+        for attr in request.form:
+            setattr(newsletter, attr, request.form[attr])
+        
+        db.session.add(newsletter)
+        db.session.commit()
+        
+        return make_response(newsletter.to_dict(), 200)
+    
+    def delete(self, id):
+        db.session.delete(Newsletter.query.filter(Newsletter.id == id))
+
+        return {{"message" : "Newsletter deleted"}, 200}
+        
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
